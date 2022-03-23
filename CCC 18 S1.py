02cd14f331452986d7ae45d06b8c9ba220738b2e
Brayden Zhang <68352655+BandB1111211331@users.noreply.github.com>
2022-03-23 16:22:48 -0400

    Add files via upload

---
+++ CCC 18 S1.py
@@ -0,0 +1,14 @@
+n = int(input())
+villages = []
+for i in range(n):
+    villages.append(int(input()))
+
+distances = []
+
+villages.sort()
+
+for i in range(1, n - 1):
+    distances.append( (villages[i+1] - villages[i]) / 2 + (villages[i] - villages[i - 1]) / 2 )
+
+distances.sort()
+print(distances[0])
